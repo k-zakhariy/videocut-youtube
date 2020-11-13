4677db81783fc8a32d058724e3e7d66d019ae446
Zakhariy <kzm.pianist@gmail.com>
2020-11-13 10:56:30 +0200

    Initial commit

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-# videocut-youtube
+# Cut youtube video by entered range
+
+### Note: `youtube-dl` should be installed
+
+------
+
+### Using
+Run `sh dl.sh` and follow the instructions
+
---
+++ dl.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+echo Введіть посилання на відео
+read youtubeUrl
+echo Введіть початок відео хв:сек
+read start
+echo Введіть кінець відео хв:сек
+read end
+youtube-dl -U
+youtube-dl -f 'bestvideo[ext=mp4]+bestaudio[ext=m4a]/bestvideo+bestaudio' --merge-output-format mp4 --output "downloaded_video.mp4" $youtubeUrl
+ffmpeg -ss 00:$start.00 -i "downloaded_video.mp4" -to 00:$end.00 -c copy "downloaded_video_cut.mp4"
+
