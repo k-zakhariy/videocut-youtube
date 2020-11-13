c151ce59fe9fe7d81d1ae94fdc82b51b62e58c8b
Zakhariy <kzm.pianist@gmail.com>
2020-11-13 18:32:15 +0200

    Fix

--- dl.sh
+++ dl.sh
@@ -3,9 +3,11 @@ echo Введіть посилання на відео
 read youtubeUrl
 echo Введіть початок відео хв:сек
 read start
-echo Введіть кінець відео хв:сек
+echo Введіть тривалість хв:сек
 read end
 youtube-dl -U
 youtube-dl -f 'bestvideo[ext=mp4]+bestaudio[ext=m4a]/bestvideo+bestaudio' --merge-output-format mp4 --output "downloaded_video.mp4" $youtubeUrl
-ffmpeg -ss 00:$start.00 -i "downloaded_video.mp4" -to 00:$end.00 -c copy "downloaded_video_cut.mp4"
+ffmpeg -ss "00:$start" -i "downloaded_video.mp4" -to "00:$end" -c copy "downloaded_video_cut.mp4"
+
+
 
